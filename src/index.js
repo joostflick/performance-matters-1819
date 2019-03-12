ed07a69b3042722ce5d2f7b0ad5bb271e8d727d0
Joost Flick <joostflick@gmail.com>
2019-03-12 13:00:28 +0100

    init structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, '/public/')))
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('Werkt'))
+
+app.get('/books/:frabl/img/:img', function(req, res) {
+  res.send(req.params)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
