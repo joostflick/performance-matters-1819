2cb826cc4b5fb343a6178ae929da1763391df4d4
Joost Flick <joostflick@gmail.com>
2019-03-13 14:45:09 +0100

    added rendering of pages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,98 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 app.use(express.static(path.join(__dirname, '/public/')))
 
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', (req, res) => res.send('Werkt'))
-
-app.get('/books/:frabl/img/:img', function(req, res) {
-  res.send(req.params)
-})
+app.set('views', path.join(__dirname, '/public/views'))
+app.get('/', (req, res) =>
+  api.getAll().then(data => {
+    const names = data[0]
+    const insults = data[1]
+    for (let i = 0; i < insults.length; i++) {
+      names[i].insult = insults[i]
+      names[i].id = i
+    }
+    res.render('pages/index', { names: names })
+  })
+)
+app.get('/details/:id', (req, res) =>
+  api
+    .getDetails(req.params.id)
+    .then(data => res.render('pages/details', { data: data[req.params.id] }))
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const api = {
+  getAll: () => {
+    return Promise.all([api.loadNames, api.loadInsults])
+  },
+  getDetails: () => {
+    return api.loadNames
+  },
+  loadInsults: new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest()
+    const link = 'https://api.whatdoestrumpthink.com/api/v1/quotes'
+    request.open('GET', link, true)
+
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        const data = JSON.parse(request.responseText)
+        resolve(data.messages.personalized)
+      } else {
+        // We reached our target server, but it returned an err
+        reject(error)
+      }
+    }
+
+    request.onerror = () => {
+      // There was a connection error of some sort
+      console.log('insults gaat fout')
+    }
+
+    request.send()
+  }),
+  loadNames: new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest()
+    const linkNames = 'https://randomuser.me/api/?results=573'
+    request.open('GET', linkNames, true)
+
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        const data = JSON.parse(request.responseText)
+        // const randomNames = []
+        // data.results.forEach(element => {
+        //   randomNames.push(element.name.first)
+        // })
+        const users = data.results.map(user => ({
+          name:
+            user.name.first.charAt(0).toUpperCase() + user.name.first.slice(1),
+          lastName:
+            user.name.last.charAt(0).toUpperCase() + user.name.last.slice(1),
+          title: user.name.title,
+          email: user.email,
+          cellphone: user.cell,
+          gender: user.gender,
+          picture: user.picture,
+          location: user.location
+        }))
+        resolve(users)
+      } else {
+        // We reached our target server, but it returned an error
+        reject(error)
+        console.log('names gaat fout')
+      }
+    }
+
+    request.onerror = () => {
+      // There was a connection error of some sort
+    }
+
+    request.send()
+  })
+}
